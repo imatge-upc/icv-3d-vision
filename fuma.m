f4bf3340895bd280c58d3ec11ee9b2d00018e467
obonnema <urama84@gmail.com>
2016-07-06 13:08:12 +0000

    compute Rotation and Translation

--- fuma.m
+++ fuma.m
@@ -1,9 +1,10 @@
-clear;
-close all;
+ clear;
+ close all;
 im1 = imread('0000_s.png');
 im2 = imread('0001_s.png');
 %give matching point
-
+f1 =0.5;
+f2 = 0.15;
 %subplot (1,2,1);
 
 %subplot (1,2,2);
@@ -13,9 +14,7 @@ matching_points_1=[];
 matching_points_2=[];
 while true
     i = i+1;
-    
-    
-    
+      
     if mod (i,2) == 1
         imshow (im1);
         disp('click on matching points in the two pictures. Right click when finish');
@@ -39,3 +38,15 @@ while true
     end
 end
 [F,inliersIndex] = estimateFundamentalMatrix(matching_points_1,matching_points_2,'Method','MSAC');
+K = [2362.12 0 1520.69; 0 2366.12 1006.81; 0 0 1];
+scale = 0.3;
+H = [scale 0 0; 0 scale 0; 0 0 1];
+K = H * K;
+
+E = K'*F*K;
+[U,S,V] = svd(E);
+Tra = V(:,3);
+R = U*[0 1 0;1 0 0;0 0 1]*V';
+if det (R) ==-1
+    R = -R;
+end

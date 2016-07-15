2e6b169dc2602f43b48a531f1ee1baf1f854d1db
obonnema <urama84@gmail.com>
2016-07-15 15:44:19 +0200

    Add files via upload

--- auto_rot_tra.m
+++ auto_rot_tra.m
@@ -1,10 +1,10 @@
- clear;
- close all;
+clear;
+close all;
 im1 = imread('0000_s.png');
 im2 = imread('0001_s.png');
- K = [2362.12 0 1520.69; 0 2366.12 1006.81; 0 0 1];
- scale = 0.3;
- H = [scale 0 0; 0 scale 0; 0 0 1];
+K = [2362.12 0 1520.69; 0 2366.12 1006.81; 0 0 1];
+scale = 0.3;
+H = [scale 0 0; 0 scale 0; 0 0 1];
 %K= [2759.48 0 1520.69;0 2764.16 1006.81;0 0 1 ];
 K =H*K;
 
@@ -26,47 +26,46 @@ viewId = 1;
 %    'Location', [0 0 0]);
 
 
-    currPoints   = detectSURFFeatures(I2, 'NumOctaves', 8);
-    currFeatures = extractFeatures(I2, currPoints, 'Upright', true);
-    indexPairs = matchFeatures(prevFeatures, currFeatures, ...
-        'MaxRatio', .7, 'Unique',  true);
-    
-        % Select matched points.
-    matchedPoints1 = prevPoints(indexPairs(:, 1));
-    matchedPoints2 = currPoints(indexPairs(:, 2));
-    F = estimateFundamentalMatrix(matchedPoints1, matchedPoints2,'Method','MSAC');
-  E = K'*F*K;
-  [U,S,V] = svd(E);
-  CameraParams = cameraParameters('IntrinsicMatrix',K);
-  
-  
+currPoints   = detectSURFFeatures(I2, 'NumOctaves', 8);
+currFeatures = extractFeatures(I2, currPoints, 'Upright', true);
+indexPairs = matchFeatures(prevFeatures, currFeatures, ...
+    'MaxRatio', .7, 'Unique',  true);
+
+% Select matched points.
+matchedPoints1 = prevPoints(indexPairs(:, 1));
+matchedPoints2 = currPoints(indexPairs(:, 2));
+F = estimateFundamentalMatrix(matchedPoints1, matchedPoints2,'Method','MSAC');
+E = K'*F*K;
+[U,S,V] = svd(E);
+CameraParams = cameraParameters('IntrinsicMatrix',K);
+
+
 % Tra = V(:,3);
 % Rot = U*[0 1 0;1 0 0;0 0 1]*V';
 % if det (Rot) ==-1
 %     Rot(:,3) = -Rot(:,3);
 % end
-     [Rot, Tra] = cameraPose2(F,CameraParams,CameraParams,matchedPoints1, matchedPoints2);
-      
-      Tra = (-Tra*Rot^-1)';
-Rot = Rot';      
-% 
+[Rot, Tra] = cameraPose2(F,CameraParams,CameraParams,matchedPoints1, matchedPoints2);
+Tra = Tra';
+
+%
 
 % orientation = R';
 % location = -t * orientation;
 
 %     % Add the current view to the view set.
 %     %vSet = addView(vSet, 2, 'Points', currPoints);
-% 
+%
 %     % Store the point matches between the previous and the current views.
 %     %vSet = addConnection(vSet, 1, 2, 'Matches', indexPairs(inlierIdx,:));
-%     
+%
 %      % Get the table containing the previous camera pose.
 %     %prevPose = poses(vSet, 1);
 %     prevOrientation = eyes(3);
 %     prevLocation    = zeros(3,1);
-%     
+%
 %      % Compute the current camera pose in the global coordinate system
 %     % relative to the first view.
 %     Rot = relativeOrient;
 %     Tra    = relativeLoc;
-%     
+%
